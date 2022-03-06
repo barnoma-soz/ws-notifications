d35dd9bdec8bcedcc6b44bbb067f6240183801e9
Akhmedov Farkhod <a.farkhod@gmail.com>
2022-03-06 22:37:00 +0500

    Добавлена разметка для форм

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <div class="flex items-start justify-center min-h-screen">
+    <div class="container p-10">
+      <div class="space-y-3">
+        <div>
+          <label for="access-token">Access Token</label>
+          <textarea id="access-token" class="form-textarea w-2/3" placeholder="Access Token"></textarea>
+        </div>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        <button type="button" class="btn-primary">Подписаться</button>
+      </div>
+
+      <h3 class="text-2xl font-semibold mt-12">Отправка тестового сообщения</h3>
+
+      <div class="space-y-3">
+        <div>
+          <label for="subject">Тема</label>
+          <input type="text" id="subject" class="form-input w-1/3" />
+        </div>
+
+        <div>
+          <label for="message">Сообщение</label>
+          <textarea id="message" class="form-textarea w-2/3" placeholder="Текст сообщения"></textarea>
+        </div>
+        
+        <button type="button" class="btn-primary">Отправить</button>
+      </div>
+    </div>
+  </div>
+</template>
